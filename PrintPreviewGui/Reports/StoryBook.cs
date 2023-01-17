1af9753bf7f35d21926911b0810514921dfc45d3
smsaleem <smsaleem2000@hotmail.com>
2023-01-17 15:38:22 +0500

    Add files via upload

---
+++ PrintPreviewGui/Reports/StoryBook.cs
@@ -0,0 +1,11 @@
+﻿namespace Sherman.WpfReporting.Gui.Reports
+{
+    public class StoryBook
+    {
+        public string Title { get; set; }
+        public string Story { get; set; }
+
+        public string URL { get; set; }
+        
+    }
+}
---
+++ PrintPreviewGui/Reports/StoryBookView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+using Sherman.WpfReporting.Gui.ViewModels;
+
+namespace Sherman.WpfReporting.Gui.Reports
+{
+    /// <summary>
+    /// Interaction logic for StoryBookView.xaml
+    /// </summary>
+    public partial class StoryBookView : UserControl
+    {
+        public StoryBookView(StoryBookViewModel vm )
+        {
+            InitializeComponent();
+            this.DataContext = vm;
+        }
+    }
+}
